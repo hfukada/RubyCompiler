d781b0afefcd736efdfb92eb0e100138bb238854
hiroshi <hiroshi@fukada.us>
2013-09-20 08:06:09 +0000

    damn. followset still doesn't work

--- src/grammar.rb
+++ src/grammar.rb
@@ -13,7 +13,7 @@ class Grammar
     INTLITERAL = /^([0-9]+)$/
     FLOATLITERAL = /^([0-9]*\.[0-9]+)$/
     SINGLEOP = /^(:|\+|-|\*|\/|!|<|>|\(|\)|;|,|\n|'|"| |=)$/
-    TERMINAL = /^(PROGRAM|BEGIN|END|FUNCTION|READ|WRITE|IF|ELSIF|ENDIF|DO|WHILE|CONTINUE|BREAK|RETURN|INT|VOID|STRING|FLOAT|TRUE|FALSE|:=|\+|-|\*|\/|=|!=|<|>|\(|\)|;|,|<=|>=)$/
+    TERMINALS = /^(PROGRAM|BEGIN|END|FUNCTION|READ|WRITE|IF|ELSIF|ENDIF|DO|WHILE|CONTINUE|BREAK|RETURN|INT|VOID|STRING|FLOAT|TRUE|FALSE|:=|\+|-|\*|\/|=|!=|<|>|\(|\)|;|,|<=|>=)$/
     DESCRIBERS = /^(INTLITERAL|FLOATLITERAL|STRINGLITERAL|IDENTIFIER)$/
 
     DEFINITIONS = {
@@ -21,27 +21,26 @@ class Grammar
     "id"               => "IDENTIFIER",
     "pgm_body"         => "decl func_declarations",
 
-    "decl"             => "STRING string_decl_list decl_tail|INT var_decl_list decl_tail|FLOAT var_decl_list decl_tail|empty",
-    "decl_tail"        => "decl|empty",
+    "decl"             => "string_decl_list decl|var_decl_list decl|empty",
 
-    "string_decl_list" => "string_decl string_decl_tail",
+    "string_decl_list" => "STRING string_decl string_decl_tail",
     "string_decl_tail" => "STRING string_decl string_decl_tail|empty",
-    "string_decl"      => "id :=> STRINGLITERAL ;|empty",
+    "string_decl"      => "id := STRINGLITERAL ;|empty",
 
     "var_decl_list"    => "var_decl var_decl_tail",
     "var_decl_tail"    => "FLOAT var_decl var_decl_tail|INT var_decl var_decl_tail|empty",
-    "var_decl"         => "id_list ;|empty", # or empty
+    "var_decl"         => "FLOAT id_list ;|INT id_list ;|empty", # or empty
     "id_list"          => "id id_tail", #id id_tail
     "id_tail"          => ", id id_tail|empty", #, id id_tail | empty
-    "var_type"         => "FLOAT|INT",
     "any_type"         => "FLOAT|INT|VOID", #
 
     "param_decl_list"  => "param_decl param_decl_tail",
     "param_decl_tail"  => ", param_decl param_decl_tail|empty", # or empty
-    "param_decl"       => "FLOAT id|INT id",  # var_type id
+    "param_decl"       => "FLOAT id|INT id",
 
-    "func_declarations"=> "func_decl func_decl_tail",
-    "func_decl"        => "FUNCTION any_type id ( parameter_decl_list ) BEGIN func_body END|FUNCTION any_type id ( ) BEGIN func_body END|empty",
+    "func_declarations"=> "func_decl func_decl_tail|empty",
+    "func_decl"        => "FUNCTION any_type id ( func_decl_param",
+    "func_decl_param"  => "param_decl_list ) BEGIN func_body END|) BEGIN func_body END",
     "func_decl_tail"   => "func_decl func_decl_tail|empty",
     "func_body"        => "decl stmt_list",
 
@@ -50,7 +49,7 @@ class Grammar
     "base_stmt"        => "assign_stmt|read_stmt|write_stmt|return_stmt",
 
     "assign_stmt"      => "assign_expr ;",
-    "assign_expr"      => "id :=> expr",
+    "assign_expr"      => "id := expr",
     "read_stmt"        => "READ ( id_list ) ;",
     "write_stmt"       => "WRITE ( id_list ) ;",
     "return_stmt"      => "RETURN expr ;",
@@ -62,8 +61,8 @@ class Grammar
     "factor_tail"      => "mulop postfix_expr factor_tail|empty",
 
     "postfix_expr"     => "primary|call_expr",
-    "call_expr"        => "id ( expr_list )|id ( )",
-    "expr_list"        => "expr expr_list_tail",
+    "call_expr"        => "id ( expr_list )",
+    "expr_list"        => "expr expr_list_tail|empty",
     "expr_list_tail"   => ", expr expr_list_tail|empty",
     "primary"          => "( expr )|id|INTLITERAL|FLOATLITERAL",
     "addop"            => "+|-",
--- src/micro.rb
+++ src/micro.rb
@@ -1,157 +1,265 @@
 #!/usr/bin/env ruby
 require './src/grammar'
+#require 'debugger'; debugger
 
 def main()
   infile = File.new(ARGV[0], "r")
-  parseStack = Grammar::DEFINITIONS["program"].split
-
+  rc = RubyCompiler.new
+  rc.createParseTable()
   infile.each_line {|line|
     cleanline = (line.include?('--')? line[0..line.index('--')-1] : line)
     cleanline.lstrip! 
-    tokens = tokenizeLine(cleanline)
+    puts "cleanline: #{cleanline}"
+    tokens = rc.tokenizeLine(cleanline)
     tokens.each{|tok|
       # tok[0] info
       # tok[1] value
-      puts "#{tok[0]}: #{tok[1]}"
-      cur = parseStack[0]
-      valid, parseStack = processSingleToken(parseStack, tok)
-      if valid
-        puts "tok[0]} has passed"
-      else
-        puts "tok[0]} did not pass"
-      end
+      #puts "#{tok[0]}: #{tok[1]}"
+      #cur = parseStack[0]
+      tok = tok[1] =~ Grammar::TERMINALS ? tok[1] : tok[0]
+      valid = rc.processSingleToken(tok.strip)
+      #if vali
+      #  puts "tok[0]} has passed"
+      #else
+      #  puts "tok[0]} did not pass"
+      #end
     }
   }
   infile.close
 end
 
-def tokenizeLine(cleanline)
-  currFront,i = 0,0
-  found=false
-  tokens = []
-  while i < cleanline.length
-    found = false
-    if !(cleanline[i] =~ (Grammar::SINGLEOP)).nil?
-      basetok = cleanline[currFront..i-1]
+class RubyCompiler
+  @parseStack
+  @parseTable
+  @indexedGrammar
 
-      if !(basetok =~ /^;/).nil?
-        tokens << [ "OPERATOR", ";" ]
-        break
-      end
-      if (cleanline[i] =~ /"|'/).nil?
-        if !(basetok.strip =~ (Grammar::KEYWORD)).nil?
-          tokens << [ "KEYWORD", "#{basetok.strip}" ]
-          found = true
-        elsif (basetok.strip =~ (Grammar::OPERATOR)).nil?
-          if !(basetok.strip =~ (Grammar::INTLITERAL)).nil?
-            tokens << [ "INTLITERAL", "#{basetok.strip}" ]
-          elsif !(basetok.strip =~ (Grammar::FLOATLITERAL)).nil?
-            tokens << [ "FLOATLITERAL", "#{basetok.strip}" ]
-          else
-            tokens << [ "IDENTIFIER", "#{basetok.strip}" ] if basetok.strip.length > 0
+  def initialize
+    @parseStack = Grammar::DEFINITIONS["program"].split 
+    @parseTable = {}
+    @indexedGrammar = []
+  end
 
+  # method to tokenize a line
+  def tokenizeLine(cleanline)
+    currFront,i = 0,0
+    found=false
+    tokens = []
+    while i < cleanline.length
+      found = false
+      if !(cleanline[i] =~ (Grammar::SINGLEOP)).nil?
+        basetok = cleanline[currFront..i-1]
+
+        if !(basetok =~ /^;/).nil?
+          tokens << [ "OPERATOR", ";" ]
+          break
+        end
+        if (cleanline[i] =~ /"|'/).nil?
+          if !(basetok.strip =~ (Grammar::KEYWORD)).nil?
+            tokens << [ "KEYWORD", "#{basetok.strip}" ]
+            found = true
+          elsif (basetok.strip =~ (Grammar::OPERATOR)).nil?
+            if !(basetok.strip =~ (Grammar::INTLITERAL)).nil?
+              tokens << [ "INTLITERAL", "#{basetok.strip}" ]
+            elsif !(basetok.strip =~ (Grammar::FLOATLITERAL)).nil?
+              tokens << [ "FLOATLITERAL", "#{basetok.strip}" ]
+            else
+              tokens << [ "IDENTIFIER", "#{basetok.strip}" ] if basetok.strip.length > 0
+
+            end
+            found = true
           end
+          if cleanline[i] != ' '
+            if !(cleanline[i..i+1] =~ (Grammar::OPERATOR)).nil?
+              tokens << ["OPERATOR" , "#{cleanline[i..i+1]}"]
+              i += 1
+            elsif !(cleanline[i] =~ (Grammar::OPERATOR)).nil? 
+              tokens << ["OPERATOR", "#{cleanline[i]}"]
+            end
+            found = true
+          end
+        else
+          endQuo = cleanline.index(/'|"/,i+1)
+          tokens << [ "STRINGLITERAL", "#{cleanline[i..endQuo]}"]
+          i = endQuo
           found = true
         end
-        if cleanline[i] != ' '
-          if !(cleanline[i..i+1] =~ (Grammar::OPERATOR)).nil?
-            tokens << ["OPERATOR" , "#{cleanline[i..i+1]}"]
-            i += 1
-          elsif !(cleanline[i] =~ (Grammar::OPERATOR)).nil? 
-            tokens << ["OPERATOR", "#{cleanline[i]}"]
-          end
-          found = true
+      end
+      i += 1 
+      if found
+        currFront = i
+      end
+    end
+    tokens
+  end
+
+  def createParseTable()
+    firstSet={}
+    Grammar::DEFINITIONS.each{|symbol, interp|
+      @parseTable[symbol] ={}
+      firstSet[symbol] = getFirstSet(symbol,[symbol]).uniq
+      puts "First(#{symbol}) = #{firstSet[symbol]}"
+    }
+    followSet={}
+    Grammar::DEFINITIONS.each{|symbol, interp|
+      followSet[symbol] = getFollowSet(firstSet,symbol,[symbol]).uniq
+      puts "Follow(#{symbol}) = #{followSet[symbol]}"
+    }
+    Grammar::DEFINITIONS.each{|symbol, interp|
+      interp.split('|').each{|choice|
+        @indexedGrammar.push({:symbol => symbol, :interp=>choice, :firstSet => firstSet[symbol], :followSet => followSet[symbol] })
+      }
+    }
+    (0..@indexedGrammar.size-1).each{|i|
+      symbol = @indexedGrammar[i][:symbol]
+      interp = @indexedGrammar[i][:interp]
+      firstSeti = @indexedGrammar[i][:firstSet]
+      followSeti = @indexedGrammar[i][:followSet]
+     # @indexedGrammar[i][:firstSet].each{|tok|
+     #   puts "#{symbol} x #{tok} -> #{i}| #{interp}"
+     #   @parseTable[symbol][tok] = @parseTable[symbol][tok].nil? ? i : @parseTable[symbol][tok]
+     # }
+
+      tokens = interp.split
+      if interp == "empty"
+        if @indexedGrammar[i][:followSet] != nil
+          @indexedGrammar[i][:followSet].each{|tok|
+            @parseTable[symbol][tok] = @parseTable[symbol][tok].nil? ? i : @parseTable[symbol][tok]
+          }
         end
       else
-        endQuo = cleanline.index(/'|"/,i+1)
-        tokens << [ "STRINGLITERAL", "#{cleanline[i..endQuo]}"]
-        i = endQuo
-        found = true
+        if !firstSet[tokens[0]].nil?
+          firstSet[tokens[0]].each{|tok|
+            #puts "#{symbol} x #{tok} -> #{i}| #{interp}"
+            @parseTable[symbol][tok] = i
+          }
+        else
+          #puts "#{symbol} x #{firstSet[tokens[0]]} -> #{i}| #{interp}"
+          @parseTable[symbol][tokens[0]] = i
+        end
+       # firstSet[tokens[0]].each{|tok|
+       #   if Grammar::TERMINAL_LIST.include?(tokens[0])
+       #     puts "specify"
+       #     puts "#{symbol} x #{tokens[0]} -> #{i}| #{interp}"
+       #     @parseTable[symbol][tokens[0]] = i
+       #   elsif @indexedGrammar[i][:firstSet].include?(tok)
+       #     puts "specify"
+       #     puts "#{symbol} x #{tokens[0]} -> #{i}| #{interp}"
+       #     @parseTable[symbol][tok] = i
+       #   end
+       # }
       end
-    end
-    i += 1 
-    if found
-      currFront = i
-    end
-  end
-  tokens
-end
-
-def createParseTable()
-  parseTable = {}
-  Grammar::DEFINITIONS.each{|symbol, interp|
-    Grammar::TERMINALS_LIST.each{|term|
-      parseTable[symbol]={term => 0}
+      @indexedGrammar[i][:firstSet].each{|tok|
+        if @parseTable[symbol][tok].nil?
+          @parseTable[symbol][tok] = i
+        end
+      }
     }
-  }
-  firstset={}
-  Grammar::DEFINITIONS.each{|symbol, interp|
-    firstSet[symbol] = getFirstSet(symbol, symbol)
-  }
-  followset={}
-  Grammar::DEFINITIONS.each{|symbol, interp|
-    choices = interp.split('|')
+    #printParseTable(parseTable)
+    #puts "#{@parseTable}"
+    @parseTable.each{|key, value|
+      @parseTable[key].each{|symbol, inter|
+        puts "#{key} x #{symbol} -> #{inter}"
+      }
+    }
+  end
+
+   # fetch firstset for a single symbol
+  def getFirstSet(symbol,taboo)
+    choices = Grammar::DEFINITIONS[symbol].split('|')
+    firstset = []
     choices.each{|choice|
-      tokens = choice.split
-      tokens.each{|tok|
-
+      break if choice == "empty"
+      tokens = choice.split(' ')
+      tokens.each{|token|
+      if !(taboo.include?(token))
+        if Grammar::DEFINITIONS.include?(token)
+          if Grammar::DEFINITIONS[token].include?("empty")
+            firstset += getFirstSet(token, taboo+[token])
+          else
+            firstset += getFirstSet(token, taboo+[token])
+            break
+          end
+        else
+          firstset += [token]
+          break
+        end
+      end
       }
     }
-  }
-
-end
-def getFirstSet(topsymbol, symbol)
-  choices = Grammar::DEFINITIONS[symbol].split('|')
-  choices.each{|choice|
-    tokens = choice.split(' ')
-    if Grammar::DEFINITIONS.include?(tokens[0])
-      firstset + getFirstSet(topsymbol,tokens[0])
-    else
-      return [tokens[0]]
-    end
-  }
-  return firstset
-end
-def getFollowSet(topsymbol, symbol)
-end
-# parseStack, the current stack string we are looking at
-# token, (token[0] info:token[1] value)
-def processSingleToken(parseStack, token)
-  return false if parseStack.size > 10
-  popped = parseStack.shift
-  currStackState = parseStack
-  result = false
-  if ( popped =~ Grammar::TERMINAL or popped =~ Grammar::DESCRIBERS)
-    # If the grammar isn't literal, then it must match a keyword or operation verbosely; token[1]
-    # if the grammar is literal, then it only need to match the token type; token[0]
-    puts "#{popped} TERMINAL"
-    return ( (!(Grammar::DESCRIBERS =~ popped) and ( popped == token[1] ) ) or ( (Grammar::DESCRIBERS =~ popped) and token[0] == popped) ), parseStack
-  else
-    # for each of the choices (in an OR situation), split, and decide
-    puts "#{popped} -> #{token[1]}"
-    choices = Grammar::DEFINITIONS[popped].split('|')
-    choices[-1] = "" if choices[-1] == "empty"
-    # if one of the pieces is matched in the newly generated string, choose that path over others.
-    if (choices.index(token[0]).nil? == false or choices.index(token[1]).nil? == false)
-      expr = choices[choices.index(token[0]).nil? ? choices.index(token[1]) : choices.index(token[0])]
-      parseStack = currStackState
-      expr_tokens = expr.split(' ')
-      expr_tokens.each { |expr_tok|
-        parseStack.unshift expr_tok
-      }
-      result,parseStack = processSingleToken(parseStack, token)
-    else
-      # all else fails, brute force
-      choices.each { |expr|
-        parseStack = currStackState
-        expr_tokens = expr.split(' ')
-        expr_tokens = (expr_tokens << parseStack).flatten
-        result, parseStack = processSingleToken(expr_tokens, token)
-        return [result, parseStack] if result
-      }
-    end
+    return firstset
   end
-  return result,parseStack
+
+  # fetch followset for a single symbol. this is a pretty bad algo.
+  def getFollowSet(firstSet, symbol, taboo)
+    #puts "Taboo: #{taboo} | Symbol: #{symbol}"
+    return [""] if symbol == "program"
+    followset = []
+    Grammar::DEFINITIONS.each{|key, value|
+      if value.include? symbol
+        choices = value.split('|')
+        choices.each{|choice|
+          tokens = choice.split
+          if !tokens.index(symbol).nil?
+            nextIndex = tokens.index(symbol) + 1
+            #puts "Tokens #{tokens}, Symbol #{symbol}, taboo #{taboo}"
+            if tokens.index(symbol) == tokens.size - 1
+              if value.include?(key)
+                followset += Grammar::DEFINITIONS.include?(key) ? firstSet[key] : [tokens[nextIndex]]
+              else
+                followset += getFollowSet(firstSet, key, taboo )
+              end
+            elsif Grammar::DEFINITIONS.include?(tokens[nextIndex]) and Grammar::DEFINITIONS[tokens[nextIndex]].include?("empty")
+              followset += getFollowSet(firstSet, tokens[nextIndex], taboo )
+              followset += firstSet[tokens[nextIndex]]
+            else
+              followset += Grammar::DEFINITIONS.include?(tokens[nextIndex]) ? firstSet[tokens[nextIndex]] : [tokens[nextIndex]]
+            end
+          end
+        }
+      end
+    }
+    return followset
+  end
+
+  # parseStack, the current stack string we are looking at
+  # token, (token[0] info:token[1] value)
+  def processSingleToken(token)
+    puts "curr stack" 
+    @parseStack.each{|i| print "#{i} | "}
+    print "\n"
+    popped = ""
+    begin
+      popped = @parseStack.shift
+    end while popped == "empty"
+    if Grammar::TERMINAL_LIST.include?(popped)
+      # If the grammar isn't literal, then it must match a keyword or operation verbosely; token[1]
+      # if the grammar is literal, then it only need to match the token type; token[0]
+      puts "#{popped} TERMINAL"
+      return ( popped == token )
+    else
+      # for each of the choices (in an OR situation), split, and decide
+      # popped = a symbol/nonterminal
+      # token[1] = attempt to match
+
+      # puts "#{popped}x#{token}"
+      prediction = @parseTable[popped][token]
+      if prediction == nil
+        @parseTable.each{|key, value|
+          @parseTable[key].each{|symbol, inter|
+            puts "|#{key}| x |#{symbol}| -> |#{inter}|"
+          }
+        }
+        puts "what failed: |#{popped}| x |#{token}|"
+        exit
+      end
+      interp = @indexedGrammar[prediction][:interp]
+      @parseStack = interp.split + @parseStack
+
+      result = processSingleToken(token)
+
+    end
+    return result
+  end
 end
 
 main()
